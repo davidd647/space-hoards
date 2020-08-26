b487275c7499e32623a4d6c1e5882033934ad150
David Dales <david.c.dales@gmail.com>
2020-08-25 22:51:40 -0400

    parcel-bundler added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import $ from "jquery";
+
+// webgazer
+//   .setGazeListener(function (data, elapsedTime) {
+//     if (data == null) {
+//       return;
+//     }
+//     var xprediction = data.x; //these x coordinates are relative to the viewport
+//     var yprediction = data.y; //these y coordinates are relative to the viewport
+//     console.log(elapsedTime); //elapsed time is based on time since begin was called
+//   })
+//   .begin();
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var imgBg = document.getElementById("img-bg");
